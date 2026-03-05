84e80ece10f6db3824eefbf23cfd320465193f5d
Anderson Pham <s4094056@student.rmit.edu.au>
2026-03-06 03:05:30 +1100

    Initial commit - Flask app with email route

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello World'
+    return render_template('index.html')
+
+@app.route('/check-email', methods=['POST'])
+def check_email():
+    data = request.get_json()
+    email = data['email']
+    return jsonify({'message': f'Checking email: {email}'})
 
 if __name__ == '__main__':
     app.run(debug=True)
